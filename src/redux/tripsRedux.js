bdab43e389a4a807dd07e20d48e66a74165356b1
Wojciech Olejnik <wojciecholejnik5@gmail.com>
2020-11-01 18:26:26 +0100

    fix selectors getTripbyId and getTripsForCountry

--- src/redux/tripsRedux.js
+++ src/redux/tripsRedux.js
@@ -17,34 +17,35 @@ export const getFilteredTrips = ({trips, filters}) => {
       trips.days >= filters.duration.from && trips.days <= filters.duration.to);
   }
   // TODO - filter by tags
-
+  if(filters.tags.length != 0){
+    output = output.filter(trip => {
+      for(let tag of trip.tags) {
+        if(filters.tags.indexOf(tag) >-1)
+          return trip;
+      }
+    });
+  }
   // TODO - sort by cost descending (most expensive goes first)
 
   return output;
 };
 
 export const getTripById = ({trips}, tripId) => {
-  const filtered = trips;
 
   // TODO - filter trips by tripId
+  const filtered = trips.find(trip => trip.id == tripId);
 
-  filtered.filter(trip => {
-    return trip.id == tripId;
-  });
-
-  // filtered.filter(trip => trip.id == tripId);
-
-  console.log('filtering trips by tripId:', tripId, trips);
-  return filtered.length ? filtered[0] : {error: true};
+  console.log('filtering trips by tripId:', tripId, filtered);
+  return filtered;
 };
 
 export const getTripsForCountry = ({trips}, countryCode) => {
-  const filtered = trips;
-
   // TODO - filter trips by countryCode
 
+  const filtered = trips.filter(trip => trip.country.code == countryCode);
+
   console.log('filtering trips by countryCode:', countryCode, filtered);
-  return filtered.length ? filtered : [{error: true}];
+  return filtered;
 };
 
 /* ACTIONS */
